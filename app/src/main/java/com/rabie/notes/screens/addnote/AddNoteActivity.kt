0ae7d23f42d5360511d15cf9210f46a0a3aac6df
rabie_mac <r.mohaimane@futuretrendz.com>
2019-07-18 00:52:08 +0100

    improving design and add close button to add note activity

--- app/src/main/java/com/rabie/notes/screens/addnote/AddNoteActivity.kt
+++ app/src/main/java/com/rabie/notes/screens/addnote/AddNoteActivity.kt
@@ -6,22 +6,41 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.view.Window
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.rabie.notes.data.models.Note
 import kotlinx.android.synthetic.main.activity_add_note.*
 
 
 class AddNoteActivity : AppCompatActivity() {
-
+    val database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference("notes")
+     var noteToUpdate:Note?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.rabie.notes.R.layout.activity_add_note)
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("notes")
+         noteToUpdate = intent.getParcelableExtra<Note>("note")
+        if (noteToUpdate !== null) {
+            myRef = database.getReference("notes").child(noteToUpdate!!.id)
+            tvName.setText(noteToUpdate!!.userName)
+            tvName.isEnabled=false
+            tvTitle.setText(noteToUpdate!!.title)
+            tvNoteDesignation.setText(noteToUpdate!!.designation)
+            tvPrice.setText(noteToUpdate!!.price.toString())
+            tvComment.setText(noteToUpdate!!.description)
+        }
+        initEvents()
+
+
+    }
+    fun initEvents(){
         btnSave.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
 
@@ -33,27 +52,42 @@ class AddNoteActivity : AppCompatActivity() {
                     tvComment.text.toString()
                 )
 
+                if (noteToUpdate == null) {
+                    myRef.push().setValue(note).addOnSuccessListener {
+                        if (!this@AddNoteActivity.isDestroyed)
+                            AlertDialog.Builder(this@AddNoteActivity)
+                                .setTitle("New Note")
+                                .setMessage("Note saved Successfully !")
+                                .setPositiveButton("Ok", DialogInterface.OnClickListener { dialogInterface, i ->
+                                    tvName.text!!.clear()
+                                    tvComment.text!!.clear()
+                                    tvNoteDesignation.text!!.clear()
+                                    tvPrice.text!!.clear()
+                                    tvTitle.text!!.clear()
+                                    tvName.requestFocus()
 
-               myRef.push().setValue(note).addOnSuccessListener {
-                  if(!this@AddNoteActivity.isDestroyed)
-                   AlertDialog.Builder(this@AddNoteActivity)
-                       .setTitle("New Note")
-                       .setMessage("Note saved Successfully !")
-                       .setPositiveButton("Ok", DialogInterface.OnClickListener { dialogInterface, i ->
-                           tvName.text!!.clear()
-                           tvComment.text!!.clear()
-                           tvNoteDesignation.text!!.clear()
-                           tvPrice.text!!.clear()
-                           tvTitle.text!!.clear()
-                           tvName.requestFocus()
+                                }).create().show()
 
-                       }).create().show()
+                    }
+                } else {
+                    myRef.setValue(note).addOnSuccessListener {
+                        if (!this@AddNoteActivity.isDestroyed)
+                            AlertDialog.Builder(this@AddNoteActivity)
+                                .setTitle(" Note")
+                                .setMessage("Note updated Successfully !")
+                                .setPositiveButton("Ok", DialogInterface.OnClickListener { dialogInterface, i ->
+                                    tvName.text!!.clear()
+                                    tvComment.text!!.clear()
+                                    tvNoteDesignation.text!!.clear()
+                                    tvPrice.text!!.clear()
+                                    tvTitle.text!!.clear()
+                                    tvName.requestFocus()
+                                    finish()
 
-               }
+                                }).create().show()
 
-
-
-
+                    }
+                }
 
 
             }
@@ -70,36 +104,46 @@ class AddNoteActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-               if(p0!!.trim().length>0)
-                if (p0!!.trim().contains("@")) {
-                    val query = myRef.orderByChild("userName").startAt(p0.toString()).limitToFirst(4)
-                    query.addValueEventListener(object :ValueEventListener{
-                        override fun onCancelled(p0: DatabaseError) {
-
-                        }
-
-                        override fun onDataChange(p0: DataSnapshot) {
-                            var items = ArrayList<String>()
-                            p0.children.forEach {
-                                Log.e("autocomplete", it.toString())
-                                  val value = it.getValue(Note::class.java)
-
-                                  if (value != null) {
-                                      Log.e("read value", "Value is: " + value!!.toString())
-                                      items.add(value.userName)
-
-                                  }
+                if (p0!!.trim().length > 0)
+                    if (p0!!.trim().contains("@")) {
+                        val query = myRef.orderByChild("userName").startAt(p0.toString()).limitToFirst(4)
+                        query.addValueEventListener(object : ValueEventListener {
+                            override fun onCancelled(p0: DatabaseError) {
 
                             }
-                            val adapter =
-                                ArrayAdapter<String>(this@AddNoteActivity, android.R.layout.select_dialog_item, items)
-                            tvName.setAdapter(adapter)
 
-                        }
+                            override fun onDataChange(p0: DataSnapshot) {
+                                var items = ArrayList<String>()
+                                p0.children.forEach {
+                                    Log.e("autocomplete", it.toString())
+                                    val value = it.getValue(Note::class.java)
 
-                    })
+                                    if (value != null) {
+                                        Log.e("read value", "Value is: " + value!!.toString())
+                                        items.add(value.userName)
 
-                }
+                                    }
+
+                                }
+                                val adapter =
+                                    ArrayAdapter<String>(
+                                        this@AddNoteActivity,
+                                        android.R.layout.select_dialog_item,
+                                        items
+                                    )
+                                tvName.setAdapter(adapter)
+
+                            }
+
+                        })
+
+                    }
+            }
+
+        })
+        ivClose.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(p0: View?) {
+                finish()
             }
 
         })
--- app/src/main/java/com/rabie/notes/screens/home/NotesAdapter.kt
+++ app/src/main/java/com/rabie/notes/screens/home/NotesAdapter.kt
@@ -2,6 +2,7 @@ package com.rabie.notes.screens.home
 
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,19 +11,23 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.FirebaseDatabase
 import com.rabie.notes.R
 import com.rabie.notes.data.models.Note
+import com.rabie.notes.screens.addnote.AddNoteActivity
+import android.graphics.Color
+import java.util.*
+
 
 class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ItemViewHolder> {
 
     var notes: List<Note>
 
+    var lastName: String = ""
 
     constructor(notes: List<Note>) : super() {
-        Log.e("NotesAdapter","NotesAdapter")
+        Log.e("NotesAdapter", "NotesAdapter")
         this.notes = notes
 
     }
@@ -30,19 +35,26 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ItemViewHolder> {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ItemViewHolder {
         val layoutInflater = LayoutInflater.from(p0.getContext())
-        val layout = layoutInflater.inflate(R.layout.recycler_item, p0, false) as ViewGroup
+        val layout = layoutInflater.inflate(com.rabie.notes.R.layout.recycler_item, p0, false) as ViewGroup
         return ItemViewHolder(layout)
     }
 
     override fun getItemCount(): Int {
-        Log.e("NotesAdapter size","${notes.size}")
+        Log.e("NotesAdapter size", "${notes.size}")
         return notes.size
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, p1: Int) {
         Log.e("onBindViewHolder", p1.toString())
-        val note=notes.get(p1)
+        val note = notes.get(p1)
         holder.tvUserName.text = note.userName
+        if(note.userName.equals(lastName)){
+            holder.tvUserName.visibility=View.GONE
+        }else{
+            holder.tvUserName.visibility=View.VISIBLE
+        }
+        holder.tvUserName.setTextColor(getRandomColor())
+        lastName=note.userName
         if (!note.title.isEmpty()) {
             holder.tvTitle.text = note.title
         } else {
@@ -50,26 +62,29 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ItemViewHolder> {
         }
         holder.tvDesignation.text = note.designation
         setPriceText(holder.tvPrice, note.price)
-        holder.tvDescription.text=note.description
-        holder.ivEdite.setOnClickListener(object :View.OnClickListener{
+        holder.tvDescription.text = note.description
+        holder.ivEdite.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                Toast.makeText(p0!!.context,"edit clicked", Toast.LENGTH_SHORT).show()
+                Toast.makeText(p0!!.context, "edit clicked", Toast.LENGTH_SHORT).show()
+                val intent = Intent(holder.itemView.context, AddNoteActivity::class.java)
+                intent.putExtra("note", note)
+                holder.itemView.context.startActivity(intent)
             }
 
         })
 
-        holder.ivDelete.setOnClickListener(object :View.OnClickListener{
+        holder.ivDelete.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 AlertDialog.Builder(holder.itemView.context)
                     .setTitle("Notes")
                     .setMessage("Are you sure , you want to delete this note?")
-                    .setPositiveButton("Delete",DialogInterface.OnClickListener { dialogInterface, i ->
+                    .setPositiveButton("Delete", DialogInterface.OnClickListener { dialogInterface, i ->
                         val database = FirebaseDatabase.getInstance()
                         val myRef = database.getReference("notes")
                         myRef.child(note.id).removeValue()
-                        Toast.makeText(p0!!.context,"delete clicked", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(p0!!.context, "delete clicked", Toast.LENGTH_SHORT).show()
                     })
-                    .setNegativeButton("Cancel",DialogInterface.OnClickListener { dialogInterface, i ->
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialogInterface, i ->
                         dialogInterface.dismiss()
                     })
                     .create().show()
@@ -93,28 +108,33 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.ItemViewHolder> {
 
 
         constructor(itemView: View) : super(itemView) {
-            tvUserName = itemView.findViewById(R.id.tvName)
-            tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
-            tvDesignation = itemView.findViewById<TextView>(R.id.tvDesignation)
-            tvDescription = itemView.findViewById<TextView>(R.id.tvDescription)
-            tvPrice = itemView.findViewById<TextView>(R.id.tvPrice)
-            ivEdite = itemView.findViewById<ImageView>(R.id.ivEdit)
-            ivDelete = itemView.findViewById<ImageView>(R.id.ivDelete)
+            tvUserName = itemView.findViewById(com.rabie.notes.R.id.tvName)
+            tvTitle = itemView.findViewById<TextView>(com.rabie.notes.R.id.tvTitle)
+            tvDesignation = itemView.findViewById<TextView>(com.rabie.notes.R.id.tvDesignation)
+            tvDescription = itemView.findViewById<TextView>(com.rabie.notes.R.id.tvDescription)
+            tvPrice = itemView.findViewById<TextView>(com.rabie.notes.R.id.tvPrice)
+            ivEdite = itemView.findViewById<ImageView>(com.rabie.notes.R.id.ivEdit)
+            ivDelete = itemView.findViewById<ImageView>(com.rabie.notes.R.id.ivDelete)
         }
     }
+
     fun setPriceText(tvPrice: TextView, price: Double) {
-        tvPrice.text = price.toString()
+        tvPrice.text = " ${price.toString()} dh "
         if (price >= 0) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                tvPrice.setTextColor(tvPrice.context.resources.getColor(R.color.green, null))
+                tvPrice.setTextColor(tvPrice.context.resources.getColor(com.rabie.notes.R.color.green, null))
             else
-                tvPrice.setTextColor(tvPrice.context.resources.getColor(R.color.green))
+                tvPrice.setTextColor(tvPrice.context.resources.getColor(com.rabie.notes.R.color.green))
 
         } else {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                tvPrice.setTextColor(tvPrice.context.resources.getColor(R.color.red, null))
+                tvPrice.setTextColor(tvPrice.context.resources.getColor(com.rabie.notes.R.color.red, null))
             else
-                tvPrice.setTextColor(tvPrice.context.resources.getColor(R.color.red))
+                tvPrice.setTextColor(tvPrice.context.resources.getColor(com.rabie.notes.R.color.red))
         }
+    }
+    fun getRandomColor(): Int {
+        val rnd = Random()
+        return Color.argb(240, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
     }
 }
